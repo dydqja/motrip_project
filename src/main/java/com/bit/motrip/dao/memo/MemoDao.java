a3762320c9204147b104dfef96364ca12d27eac9
kimerum333 <kimerum333@gmail.com>
2023-05-22 20:44:23 +0900

    alex6/Update&Delete 23/05/22 20:44 MemoDao CRUD 완성 but Page처리는 아직
    MemoDao,Mapper,DaoTest : Search 와 join 을 활용한 메모리스트 조회 기능추가, delete에 유저검증기능 MemoAttach 삭제,
    
    (memo 관련 파일 전부, Search updated, MemoAttach.java 삭제)

--- src/main/java/com/bit/motrip/dao/memo/MemoDao.java
+++ src/main/java/com/bit/motrip/dao/memo/MemoDao.java
@@ -11,14 +11,22 @@ import java.util.List;
 @Mapper
 @Qualifier("memoDao")
 public interface MemoDao {
-    //add 후 memoNo를 리턴해야 함
+    //memo
     int addMemo(Memo memo) throws Exception;
     //    int addMemo(@Param("memo")Memo memo, @Param("memoAccess")MemoAccess memoAccess) throws Exception;
     Memo getMemo(int memoNo) throws Exception;
-    int getMaxMemoNo() throws Exception;
-    int getMaxMemoAccessNo() throws Exception;
-    List<Memo> getMemoList(String userId, Search search) throws Exception;
-    int deleteMemo(int memoNo) throws Exception;
+    List<Memo> getMemoList(Search search) throws Exception;
+    int deleteMemo(int memoNo,String userId) throws Exception;
     int updateMemo(Memo memo);
+
+    //memoAccess
+
+    //다른 사람에게 공유해줌
     int addMemoAccess(MemoAccess memoAccess) throws Exception;
+    //다른 사람에 대한 공유를 해제함.
+    int deleteMemoAccess(MemoAccess memoAccess) throws Exception;
+
+    List<MemoAccess> getMemoAccessListByMemoNo(int memoNo) throws Exception;
+    List<MemoAccess> getMemoAccessListByUserId(String userId) throws Exception;
+
 }
