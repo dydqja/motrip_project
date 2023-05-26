9d2a413dd767c00717aa8b2908233825c5277ac7
kimerum333 <kimerum333@gmail.com>
2023-05-26 18:20:30 +0900

    alex6/Update 23/05/26 18:20 써머노트 이미지저장기 초벌 제작
    ImageSaver.java : 각 타입을 넣었을 때 저장하고 도로뱉도록 해야함..
    
    (ImageSaver.java)

--- src/main/java/com/bit/motrip/common/ImageSaver.java
+++ src/main/java/com/bit/motrip/common/ImageSaver.java
@@ -1,18 +1,56 @@
 package com.bit.motrip.common;
 
+import com.bit.motrip.domain.Memo;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Component;
+import org.springframework.util.FileCopyUtils;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Base64;
+
+import java.io.File;
+import java.util.List;
+
+@Component
 public class ImageSaver {
     //constructor
-    public ImageSaver() {
-
+    public ImageSaver() throws IOException {
     }
+
+
     ///field
-    private String pathPrefix;
+    public static String path1 = "/static/images/";
+    public static String path2 = "C:\\mainproject\\motrip_main_project\\static\\images\\mountpoint\\";
 
     //getter & setter
-    public String getPathPrefix() {
-        return pathPrefix;
+
+    //method
+    public void imageSave(byte[] imageFile, String filePath, String fileName) throws IOException {
+        //저장경로 설정해주는곳
+        String prefix = filePath;
+        File targetFile = new File(prefix + fileName);
+        FileCopyUtils.copy(imageFile, targetFile);
     }
-    public void setPathPrefix(String pathPrefix) {
-        this.pathPrefix = pathPrefix;
+
+    public void imageSave(byte[] imageFile, String fileName) throws IOException {
+        //저장경로 설정해주는곳
+        String prefix = path2;
+        File targetFile = new File(prefix + fileName);
+        FileCopyUtils.copy(imageFile, targetFile);
+
     }
+    public Memo imageSave(Memo memo) throws Exception{
+        String htmlContents = memo.getMemoContents();
+        List<String> imageStrings = new ArrayList<>();
+        //파싱해서 이미지를 저장하는 부분
+        Document doc = Jsoup.parse(htmlContents);
+        //이미지 부분을 다른 글귀로 변경하는 부분
+
+        //변경된 메모를 리턴하는 부분
+        return memo;
+    }
+
 }
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -50,6 +50,16 @@
         </a>
     </div>
 
+    <div>
+        <p>
+            이미지 테스트용
+            이것은 프로젝트 폴더 내부의 이미지를 불러오는 것입니다.
+            <img src="images/vpvp.jpg" width="100" height="100">
+            이것은 업로드되는 이미지를 불러오는 것입니다.
+            <img src="images/mountpoint/pika.jpg" width="100" height="100">
+        </p>
+    </div>
+
 
 </body>
 </html>
