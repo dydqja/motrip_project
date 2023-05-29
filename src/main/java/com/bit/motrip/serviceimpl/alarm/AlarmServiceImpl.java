82bc9ae6d8910030a81a516db72e51654b9b189e
ksg <kimerum333@gmail.com>
2023-05-29 11:00:53 +0900

    alex6/Create 23/05/28 18:56 AlarmService 틀만 생성

---
+++ src/main/java/com/bit/motrip/serviceimpl/alarm/AlarmServiceImpl.java
@@ -0,0 +1,35 @@
+package com.bit.motrip.serviceimpl.alarm;
+
+import com.bit.motrip.domain.Alarm;
+import com.bit.motrip.service.alarm.AlarmService;
+
+import java.util.Map;
+
+public class AlarmServiceImpl implements AlarmService {
+
+    @Override
+    public int addAlarm(Alarm alarm) {
+
+        return 0;
+    }
+
+    @Override
+    public int deleteAlarm(int alarmNo) {
+        return 0;
+    }
+
+    @Override
+    public int removeAlarm(int alarmNo) {
+        return 0;
+    }
+
+    @Override
+    public Alarm getAlarm(int alarmNo) {
+        return null;
+    }
+
+    @Override
+    public Map<String, Object> getAlarmListCount(String userId) {
+        return null;
+    }
+}
