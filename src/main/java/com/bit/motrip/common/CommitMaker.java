e5e3effbbf9f95162cae066b3af48207ced49a41
kimerum333 <kimerum333@gmail.com>
2023-06-01 09:22:03 +0900

    alex6/Create 23/06/01 09:21 footer Memo ver 0.1 CRUD
    수정 직후 수정된 메모를 다시볼 때 div에 반영사항이 안들어가는 버그 있음.부착기능, 공유기능 구현필요. 알람구현에 일단 착수하겠음
    
    (footer.jsp)

--- src/main/java/com/bit/motrip/common/CommitMaker.java
+++ src/main/java/com/bit/motrip/common/CommitMaker.java
@@ -28,11 +28,12 @@ public class CommitMaker {
         //                        0         1          2            3                4                5
         String[] commitType = {"Create", "Update", "Delete", "Create&Update", "Update&Delete", "Create&Delete"};
         //커밋 소제목
-        String commitTitle = "Tiles single layout 생성";
+        String commitTitle = "footer Memo ver 0.1 CRUD";
         //수정내역
-        String commitBody = ".jsp로 들어갈 시, 따로 돌아갈 수 있도록 변경";
+        String commitBody = "수정 직후 수정된 메모를 다시볼 때 div에 반영사항이 안들어가는 버그 있음." +
+                "부착기능, 공유기능 구현필요. 알람구현에 일단 착수하겠음";
         //수정파일
-        String changedFiles = "(tiles.xml, singleLayout.jsp)";
-        gitCommitMSGBuilder(committers[0],commitType[1],commitTitle,commitBody,changedFiles);
+        String changedFiles = "(footer.jsp)";
+        gitCommitMSGBuilder(committers[0],commitType[0],commitTitle,commitBody,changedFiles);
     }
 }
--- src/main/java/com/bit/motrip/restcontroller/MemoRestController.java
+++ src/main/java/com/bit/motrip/restcontroller/MemoRestController.java
@@ -1,12 +1,19 @@
 package com.bit.motrip.restcontroller;
 
-import com.bit.motrip.domain.Memo;
+import com.bit.motrip.common.Search;
+import com.bit.motrip.domain.*;
 import com.bit.motrip.service.memo.MemoService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 @CrossOrigin
 @RestController
@@ -19,17 +26,190 @@ public class MemoRestController {
     }
     //Field
     private final MemoService memoService;
+    private final String fakeJason = "{\"key1\": \"value1\", \"key2\": \"value2\", \"key3\": \"value3\"}";
 
-    //Method
-    @GetMapping("test/{id}")
-    public String test(@PathVariable("id") String id, HttpSession session)  {
+    @PostMapping("getMemoList")
+    public String getMemoList(HttpSession session,
+                              @RequestParam("searchCondition") String searchCondition,
+                              @RequestParam("currentPage") String currentPage)  {
 
+        System.out.println("레스트컨트롤러 겟메모리스트 동작");
+        //리퀘스트 파람 둘과 유저를 모아서 필요한 데이터를 모두 수집했다.
+        User user = (User) session.getAttribute("user");
+        System.out.println("받은 유저아이디는"+user.getUserId());
+        System.out.println("받은 서치컨디션은"+searchCondition);
+        System.out.println("받은 현재페이지는"+currentPage);
+        if (currentPage == null || currentPage.equals("")){
+            currentPage = "1";
+        }
 
+        //검색조건을 세팅
+        Search search = new Search();
+        search.setSearchCondition(searchCondition);
+        search.setCurrentPage(Integer.parseInt(currentPage));
+
+        //비즈니스 로직 수행
+        List<MemoDoc> memoDocList = null;
+        try {
+            System.out.println("trying to get memoDocList on MemoRestController");
+            memoDocList = memoService.getMemoList(user.getUserId(), search);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
         //json 으로 변환
         ObjectMapper objectMapper = new ObjectMapper();
-        String json = "";
+        String memoDocListJson = "";
+        try {
+            memoDocListJson = objectMapper.writeValueAsString(memoDocList);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("이것은 변환된 쩨이쓴");
+        System.out.println(memoDocListJson);
 
-        return json;
+        //세션에 현재조건을 저장
+        MemoPage memoPage = new MemoPage();
+        memoPage.setSearchCondition(searchCondition);
+        memoPage.setCurrentPage(Integer.parseInt(currentPage));
+        session.setAttribute("memoPage", memoPage);
+
+        return memoDocListJson;
+    }
+
+    @PostMapping("getMemo")
+    public String getMemo(@RequestParam("memoNo") String memoNo, HttpSession session)  {
+
+        User user = (User) session.getAttribute("user");
+        int memoNoInt = Integer.parseInt(memoNo);
+
+        System.out.println("로그인한 유저의 아이디는 "+user.getUserId()+"입니다.");
+        System.out.println("메모번호는 "+memoNo+"입니다.");
+
+        Memo memo = null;
+        try {
+            memo = memoService.getMemo(memoNoInt);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        ObjectMapper objectMapper = new ObjectMapper();
+        String memoJson = "";
+        try {
+            memoJson = objectMapper.writeValueAsString(memo);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+        return memoJson;
+    }
+    @PostMapping("updateMemo")
+    public String updateMemo(
+            HttpSession session
+            ,@RequestParam("memoNo") String memoNo
+            ,@RequestParam("memoContents") String memoContents
+            ,@RequestParam("memoTitle") String memoTitle
+            ,@RequestParam("memoDialogNo") String memoDialogNo
+             )  {
+        System.out.println("레스트컨트롤러 업데이트메모 동작");
+        System.out.println("메모번호는 "+memoNo+"입니다.");
+        System.out.println("메모제목은 "+memoTitle+"입니다.");
+        System.out.println("메모내용은 "+memoContents+"입니다.");
+        System.out.println("메모다이얼로그번호는 "+memoDialogNo+"입니다.");
+
+        User user = (User) session.getAttribute("user");
+        int memoNoInt = Integer.parseInt(memoNo);
+
+        Memo memo = new Memo();
+        memo.setMemoNo(memoNoInt);
+        memo.setMemoTitle(memoTitle);
+        memo.setMemoContents(memoContents);
+        memo.setMemoAuthor(user.getUserId());
+
+        try {
+            memoService.updateMemo(memo);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return fakeJason;
+    }
+    @PostMapping("deleteMemo")
+    public String deleteMemo(@RequestParam("memoNo") String memoNo)  {
+
+        int memoNoInt = Integer.parseInt(memoNo);
+        Memo memo = new Memo();
+        memo.setMemoNo(memoNoInt);
+
+        System.out.println("메모번호는 "+memoNo+"입니다.");
+
+        try {
+            memoService.deleteMemo(memo);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return fakeJason;
+    }
+    @PostMapping("removeMemo")
+    public String removeMemo(@RequestParam("memoNo") String memoNo)  {
+
+        int memoNoInt = Integer.parseInt(memoNo);
+        Memo memo = new Memo();
+        memo.setMemoNo(memoNoInt);
+        memo.setMemoDelDate(    new Date(System.currentTimeMillis())    );
+
+        System.out.println("메모번호는 "+memoNo+"입니다.");
+
+        try {
+            memoService.deleteMemo(memo);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        return fakeJason;
+    }
+    //restore
+    @PostMapping("restoreMemo")
+    public String restoreMemo(
+            @RequestParam("memoNo") String memoNo)  {
+
+        int memoNoInt = Integer.parseInt(memoNo);
+
+        Memo memo = new Memo();
+        memo.setMemoNo(memoNoInt);
+        memo.setMemoDelDate(new Date(System.currentTimeMillis()));
+
+        System.out.println("메모번호는 "+memoNo+"입니다.");
+
+        try {
+            memoService.restoreMemo(memo);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        return fakeJason;
+    }
+    //새 메모 생성
+    @PostMapping("addMemo")
+    public String addMemo(
+            HttpSession session,
+            @RequestParam("memoDialogNo") String memoDialogNo) {
+        System.out.println("레스트컨트롤러 addMemo 동작");
+        Memo memo = null;
+        User user = (User) session.getAttribute("user");
+        try {
+            memo = memoService.addMemo(user);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String memoJson = "";
+        try {
+            memoJson = objectMapper.writeValueAsString(memo);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+        return memoJson;
+
     }
 }
