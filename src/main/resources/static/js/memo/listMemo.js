5722cb8766219cc55a4a0c9346b2d0bf4b96e329
kimerum333 <kimerum333@gmail.com>
2023-06-08 20:19:02 +0900

    alex6/Update 23/06/08 20:18 memoList 구현중
    중간 커밋
    
    (header.jsp, ajaxMemo.js,header.jsp, memoBtnCtrl.js,memoFunction.js)

--- src/main/resources/static/js/memo/listMemo.js
+++ src/main/resources/static/js/memo/listMemo.js
@@ -1,9 +1,35 @@
 //도큐먼트에 달 리스너
 $(document).ready(function () {
     console.log("document ready");
+
+    let userId = $("#memo-user-id").val();
+    console.log("userId : " + userId);
+    if (!userId){
+        console.log("userId is undefined");
+        return;
+    }
     //최초 1회, getMemoList 를 호출하여 myMemoList 를 가져온다.
     getMemoList('myMemo');
 });
+
+function changePage(searchCondition, change){
+    let changeTarget = 0;
+    if(searchCondition == 'myMemo'){
+        changeTarget = $("#my-memo-current-page");
+    }else if(searchCondition == 'sharedMemo'){
+        changeTarget = $("#shared-memo-current-page");
+    } else {
+        changeTarget = $("#del-memo-current-page");
+    }
+    let currentPage = parseInt(changeTarget.text());
+    let changePage = currentPage + change;
+    if(changePage < 1){
+        changePage = 1;
+    }
+    changeTarget.text(changePage);
+    getMemoList(searchCondition,changePage);
+}
+
 //아이디값이 my-memo-btn, shared-memo-btn, del-memo-btn 인 세 버튼에 달린 리스너
 $(document).on("click", "#my-memo-btn, #shared-memo-btn, #del-memo-btn", function () {
     //버튼의 아이디값을 가져온다.
@@ -41,9 +67,18 @@ function getMemoList(searchCondition,currentPage){
     console.log("getMemoList on");
     console.log("searchCondition : " + searchCondition);
     console.log("currentPage : " + currentPage);
-
     console.log("위의 조건으로 ajax를 날립니다.");
-    // #my-memo-list 에 넣을 my memo 를 ajax로 받는다.
+
+    //searchCondition 에 맞는 list-container 를 잡는다.
+    let listContainer = $("#my-memo-list-container");
+    if(searchCondition == 'sharedMemo'){
+        listContainer = $("#shared-memo-list-container");
+    }else if(searchCondition == 'deletedMemo'){
+        listContainer = $("#del-memo-list-container");
+    }
+
+    //userId 를 받는다.
+    //memo 를 ajax로 받는다.
     $.ajax({
         type: "POST",
         url: "/memo/getMemoList",
@@ -53,109 +88,49 @@ function getMemoList(searchCondition,currentPage){
         success: function (response) {
             let memoDocList = response;
             console.log("memoDocList : " + memoDocList);
-            //buildMemoListThumbnail(searchCondition, memoDocList);
+            //각 memoDoc마다 썸네일을 만든다.
+            memoDocList.forEach(memoDoc => {
+               buildMemoListThumbnail(memoDoc,listContainer)
+            });
         }
     });
 }
 
-function buildMemoListThumbnail(searchCondition,memoDocList){
-    // 썸네일 만들 곳을 객체로 잡는다.
-    //기본 searchCondition 은 'myMemo'
-    let listArea = document.querySelector("#my-memo-list");
-    if(searchCondition == 'sharedMemo'){
-        listArea = document.querySelector("#shared-memo-list");
-    } else if (searchCondition == 'deletedMemo'){
-        listArea = document.querySelector("#del-memo-list");
+function buildMemoListThumbnail(memoDoc,listContainer){
+    //list-container를 비운다.
+    listContainer.html('');
+    //memoDoc의 상태를 체크한다.
+    let btnClass = "btn btn-sm btn-default";
+    let btnText = "무소속";
+    if(memoDoc.tripPlan){
+        btnClass = "btn btn-sm btn-primary";
+        btnText = memoDoc.tripPlan.tripPlanTitle;
+    }else if(memoDoc.chatRoom){
+        btnClass = "btn btn-sm btn-info";
+        btnText = memoDoc.chatRoom.chatRoomTitle;
+    }else if(memoDoc.review){
+        btnClass = "btn btn-sm btn-warning";
+        btnText = memoDoc.review.reviewTitle;
     }
-    // listArea 에 있는 모든 자식을 지운다.
-    while (listArea.hasChildNodes()){
-        listArea.removeChild(listArea.firstChild);
-    }
-    memoDocList.forEach(memoDoc => {
-        let memoDocThumbnailBtn = new MemoDocThumbnail(memoDoc.tripPlan, memoDoc.review, memoDoc.chatRoom, memoDoc.memoList).buildThumbnail();
-        listArea.appendChild(memoDocThumbnailBtn);
-        const thumbnailValueString = memoDocThumbnailBtn.value;
-        // JSON 형식의 문자열을 JavaScript 객체로 변환
-        const thumbnailValue = JSON.parse(thumbnailValueString);
-        // memoList 속성에 접근
-        const memoList = thumbnailValue.memoList;
-        // memoList 에 있는 모든 메모를 썸네일로 만들어서 썸네일 버튼에 추가
-        memoList.forEach(memo => {
-            const memoThumbnailBtn = new MemoThumbnail(memo).buildThumbnail();
-            memoDocThumbnailBtn.appendChild(memoThumbnailBtn);
-        });
+    //memoDoc를 담을 버튼을 만든다.
+    let memoDocBtn = $('<div>').addClass('my-memo-thumbnail btn-group-justified').attr('role', 'group');
+    let innerBtn = $('<a>').attr('href', '#').addClass(btnClass).attr('role', 'button').text(btnText);
+    memoDocBtn.append(innerBtn);
+    //memoDocBtn를 list-container에 붙인다.
+    listContainer.append(memoDocBtn);
+    //memoDoc의 내부 메모를 확인한다.
+    memoDoc.memoList.forEach(memo => {
+        //내부 메모의 내용을 확인한다.
+        console.log("memo : " + memo);
+        console.log("memo.memoNo : " + memo.memoNo);
+        console.log("memo.memoTitle : " + memo.memoTitle);
+        console.log("memo.memoContents : " + memo.memoContents);
+        console.log("memo.memoColor : " + memo.memoColor);
+        //내부 메모를 담을 버튼을 만든다.
+        let memoBtn = $('<div>').addClass('my-memo-thumbnail btn-group-justified').attr('role', 'group');
+        let innerBtn = $('<a>').attr('href', '#').addClass('btn btn-line btn-sm btn-default').attr('role', 'button').text(memo.memoTitle);
+        memoBtn.append(innerBtn);
+        //memoBtn를 list-container에 붙인다.
+        listContainer.append(memoBtn);
     });
-
-}
-class MemoDocThumbnail {
-    constructor(tripPlan, review, chatRoom, memoList) {
-        this.tripPlan = tripPlan;
-        this.review = review;
-        this.chatRoom = chatRoom;
-        this.memoList = memoList;
-    }
-
-    buildThumbnail() {
-        let memoDocThumbnail = document.createElement("button");
-        memoDocThumbnail.setAttribute("type", "button");
-        memoDocThumbnail.classList.add("memo-doc-thumbnail");
-        memoDocThumbnail.classList.add("btn");
-        memoDocThumbnail.classList.add("btn-line");
-        memoDocThumbnail.classList.add("btn-sm");
-        memoDocThumbnail.classList.add("btn-primary");
-        memoDocThumbnail.setAttribute("role", "group");
-
-            const thumbnailValue = {
-                tripPlan: this.tripPlan,
-                review: this.review,
-                chatRoom: this.chatRoom,
-                memoList: this.memoList
-            };
-
-            memoDocThumbnail.value = JSON.stringify(thumbnailValue);
-
-        return memoDocThumbnail;
-    }
-}
-
-class MemoThumbnail {
-    constructor(memo) {
-        this.memo = memo;
-    }
-
-    buildThumbnail() {
-        let memoThumbnail = document.createElement("button");
-        memoThumbnail.setAttribute("type", "button");
-        memoThumbnail.classList.add("memo-thumbnail");
-        memoThumbnail.classList.add("btn");
-        memoThumbnail.classList.add("btn-line");
-        memoThumbnail.classList.add("btn-sm");
-        memoThumbnail.classList.add("btn-default");
-        memoThumbnail.setAttribute("role", "group");
-
-        const thumbnailValue = {
-            memo: this.memo
-        };
-
-        memoThumbnail.value = JSON.stringify(thumbnailValue);
-
-        return memoThumbnail;
-    }
-}
-function changePage(searchCondition, change){
-    let changeTarget = 0;
-    if(searchCondition == 'myMemo'){
-        changeTarget = $("#my-memo-current-page");
-    }else if(searchCondition == 'sharedMemo'){
-        changeTarget = $("#shared-memo-current-page");
-    } else {
-        changeTarget = $("#del-memo-current-page");
-    }
-    let currentPage = parseInt(changeTarget.text());
-    let changePage = currentPage + change;
-    if(changePage < 1){
-        changePage = 1;
-    }
-    changeTarget.text(changePage);
-    getMemoList(searchCondition,changePage);
 }
