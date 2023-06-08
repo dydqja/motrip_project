4c3686ca263be146d8920abee9410256ef887a08
angie <eunjin5294@gmail.com>
2023-06-09 03:36:25 +0900

    angie/ Update 23/06/09 03:36 Review 전체적으로 수정
    공개 후기 리스트, 나의 후기 리스트 출력시 로직 다시짜고 컨트롤단도 다시 작성함. 추가로 조회수와 좋아요 컨트롤단 구현했고 화면단에서 버튼 만들 예정

--- src/main/java/com/bit/motrip/serviceimpl/review/ReviewServiceImpl.java
+++ src/main/java/com/bit/motrip/serviceimpl/review/ReviewServiceImpl.java
@@ -1,15 +1,13 @@
 package com.bit.motrip.serviceimpl.review;
-
 import com.bit.motrip.common.Search;
-import com.bit.motrip.controller.CommentController;
+import com.bit.motrip.dao.evaluateList.EvaluateListDao;
 import com.bit.motrip.dao.review.ReviewDao;
 import com.bit.motrip.dao.tripplan.TripPlanDao;
 import com.bit.motrip.dao.chatroom.ChatMemberDao;
-import com.bit.motrip.domain.ChatMember;
-import com.bit.motrip.domain.Review;
-import com.bit.motrip.domain.TripPlan;
+import com.bit.motrip.domain.*;
 import com.bit.motrip.service.review.ReviewService;
 import com.bit.motrip.service.tripplan.TripPlanService;
+import com.bit.motrip.service.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -35,12 +33,56 @@ public class ReviewServiceImpl implements ReviewService {
     @Qualifier("chatMemberDao")
     private ChatMemberDao chatMemberDao;
 
+    @Autowired
+    @Qualifier("evaluateListDao")
+    private EvaluateListDao evaluateListDao;
+
+    @Autowired
+    @Qualifier("userServiceImpl")
+    private UserService userService;
+
     public ReviewServiceImpl() {
     }
 
     //화면에 보여줄 리스트의 수
     @Value("${tripPlanPageSize}")
     private int reviewPageSize;
+
+    @Override
+    public Map<String, Object> selectReviewList(Map<String, Object> parameters) throws Exception {
+
+        Search search = (Search) parameters.get("search");
+        int offset = (search.getCurrentPage() - 1) * reviewPageSize;
+        search.setTotalCount(tripPlanDao.selectTripPlanCount()); // 여행플랜 총 카운트
+        search.setCurrentPage(search.getCurrentPage()); // 클라이언트에서 요청한 페이지 번호
+        search.setLimit(reviewPageSize); // LIMIT 값은 페이지당 항목 수와 동일합니다.
+        search.setOffset(offset);
+
+        String condition = (String) parameters.get("condition");
+
+        User dbUser = (User) parameters.get("user");
+        if ("myReviewList".equals(condition)) { // 나의 후기
+            parameters.put("reviewAuthor", dbUser.getUserId());
+            parameters.put("search", search);
+            parameters.put("reviewList", reviewDao.selectReviewList(parameters));
+            System.out.println("여기는 서비스임쁠parameters>>>>" + parameters);
+        } else if("publicReviewList".equals(condition)) { //전체 공개 후기
+            List<Review> reviewList = reviewDao.selectReviewList(parameters);//후기목록조회
+            System.out.println("서비스임쁠 reviewList>>>>>>" + reviewList);
+            List<Review> updatedReviewList = new ArrayList<>();//후기 목록을 조회하여 reviewList에 저장
+
+            for (Review review : reviewList) {
+                User user = userService.getUserById(review.getReviewAuthor());
+                review.setReviewAuthor(user.getNickname());
+                updatedReviewList.add(review);
+            }
+
+            parameters.put("reviewList", updatedReviewList);
+
+        }
+        return parameters;
+    }
+
 
 
     @Override
@@ -94,42 +136,49 @@ public class ReviewServiceImpl implements ReviewService {
         System.out.println("Review data in Service: " + review.toString());
         reviewDao.addReview(review);
     }
+/*
 
-    @Override // 공유된 여행플랜 목록
-    public Map<String, Object> selectReviewList(Search search) throws Exception {
 
+    @Override
+    public Map<String, Object> getMyReviewList(Map<String, Object> parameters) throws Exception {
+
+        Search search = (Search) parameters.get("search");
         int offset = (search.getCurrentPage() - 1) * reviewPageSize;
-
         search.setTotalCount(reviewDao.selectReviewCount()); // 여행플랜 총 카운트
         search.setCurrentPage(search.getCurrentPage()); // 클라이언트에서 요청한 페이지 번호
         search.setLimit(reviewPageSize); // LIMIT 값은 페이지당 항목 수와 동일합니다.
-        search.setOffset(offset); //
+        search.setOffset(offset);
 
-        Map<String, Object> paramaters = new HashMap<>();
-        paramaters.put("reviewAuthor", "");
-        paramaters.put("search", search);
-        paramaters.put("reviewList", reviewDao.selectReviewList(paramaters));
+        User dbUser = (User) parameters.get("user");
+        if (dbUser != null) { // 나의 여행플랜
+            parameters.put("reviewAuthor", dbUser.getUserId());
+            parameters.put("search", search);
+            parameters.put("reviewList", reviewDao.selectReviewList(parameters));
 
-        return paramaters;
+
+        } else { // 전체 여행플랜
+            List<Review> reviewList = reviewDao.selectReviewList(parameters); // nickname 필드를 별도 만들지 않았지만 플랜 작성자 정보를 통해 가져와서 넣어준다.
+            List<Review> updatedReviewList = new ArrayList<>();
+            for (Review review : reviewList) {
+                User user = userService.getUserById(review.getReviewAuthor());
+                review.setReviewAuthor(user.getNickname());
+                updatedReviewList.add(review);
+            }
+            parameters.put("reviewList", updatedReviewList);
+
+        }
+        return parameters;
     }
-
-
-    //공개된 후기 목록 조회
-    @Override
-     public List<Review> getPublicReviewList(Search search) throws Exception {
-       return reviewDao.getPublicReviewList(search);
-    }
-
-    //나의 후기 목록 조회
-    @Override
-    public List<Review> getMyReviewList(String reviewAuthor, Search search) throws Exception {
-        return reviewDao.getMyReviewList(reviewAuthor, search);
-    }
+*/
 
     //특정 후기 조회
     @Override
     public Review getReview(int reviewNo) throws Exception {
-        return reviewDao.getReview(reviewNo);
+        Review review = reviewDao.getReview(reviewNo);
+        review.setViewCount(review.getViewCount() + 1);
+
+        reviewDao.getReview(reviewNo);
+        return review;
     }
 
     //후기 수정
@@ -146,5 +195,33 @@ public class ReviewServiceImpl implements ReviewService {
     @Override
     public void recoverReview(int reviewNo) {
     }
+
+    @Override
+    public int reviewLikes(Map<String, Object> reviewLikes) throws Exception {
+        Review review = new Review();
+        EvaluateList evaluateList = new EvaluateList();
+        reviewLikes.put("searchCondition", "review"); // 여행플랜을 기준으로 찾기 위해
+
+        User dbUser = (User) reviewLikes.get("user");
+        evaluateList.setEvaluaterId(dbUser.getUserId());
+        evaluateList.setEvaluatedReviewNo((Integer) reviewLikes.get("reviewNo"));
+
+        List<EvaluateList> reviewEvaluateList = evaluateListDao.getEvaluation(reviewLikes);
+        for (int i=0; i<reviewEvaluateList.size(); i++){
+            if(reviewEvaluateList.get(i).getEvaluaterId().equals(dbUser.getUserId())){
+                System.out.println("이미 추천을 누른 후기입니다.");
+                return -1;
+            }
+        }
+        // 중복체크를 확인하여 이상이 없다면 새로운 테이블을 생성
+        evaluateListDao.addEvaluation(evaluateList);
+        // 추천수를 높이고 tripPlanEvaluateList의 숫자가 최근 추천수이기에 + 1을 더해주어 최종 저장
+        review.setReviewLikes(reviewEvaluateList.size() + 1);
+        review.setTripPlanNo((Integer) reviewLikes.get("reviewNo"));
+        reviewDao.reviewLikes(review);
+        return review.getReviewLikes();
+    }
 }
+
+
 
