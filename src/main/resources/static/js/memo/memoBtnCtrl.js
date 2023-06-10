ee8754db5ad70da37efcb75e8d8f1a51c1d76076
alex6 <kimerum333@gmail.com>
2023-06-10 18:56:10 +0900

    alex6/Update 23/06/10 18:55 메모 공유 완료
    부착까지만 하면 끝
    
    (memo 관련 파일 전부, 헤더)

--- src/main/resources/static/js/memo/memoBtnCtrl.js
+++ src/main/resources/static/js/memo/memoBtnCtrl.js
@@ -93,8 +93,8 @@ $(document).on('click', '.memo-share-modal-unShare-btn-for-sharer', function() {
     //memoNo 는 memo-sharer-list-memoNo 에 있다.
     let memoNo = $(this).closest('#memo-sharer-list-body').find('#memo-sharer-list-memoNo').val();
     let memoAccessUser = $(this).val();
-    console.log("공유해제될 memoNo : " + memoNo);
-    console.log("공유해제될 memoAccessUser : " + memoAccessUser);
+    //console.log("공유해제될 memoNo : " + memoNo);
+    //console.log("공유해제될 memoAccessUser : " + memoAccessUser);
     unshareMemoRequest(memoNo, memoAccessUser);
 });
 
@@ -103,8 +103,50 @@ $(document).on('click', '#memo-share-modal-unShare-btn-for-shared', function() {
     //unShareMemo 를 하기 위한 정보를 모은다.
     let memoNo = $("#memo-sharer-list-memoNo").val();
     let memoAccessUser = $(this).val();
-    console.log("공유해제될 memoNo : " + memoNo);
-    console.log("공유해제될 memoAccessUser : " + memoAccessUser);
+    //console.log("공유해제될 memoNo : " + memoNo);
+    //console.log("공유해제될 memoAccessUser : " + memoAccessUser);
     unshareMemoReQuestForShared(memoNo, memoAccessUser);
 });
+//new-memo-sharee-plus-btn
+$(document).on('click', '.new-memo-sharee-ajax-btn', function() {
+    console.log('#new-memo-sharee-ajax-btn clicked');
+    event.preventDefault();
+    //공유자를 추가하기 위한 정보를 모은다.
+    //memoNo 는 이 버튼의 value 다.
+    let memoNo = $(this).val();
+    //console.log('memoNo ='+memoNo)
+    //nickname 는 가장 가까운 new-memo-sharee-input의 값이다.
+    let nickname = $(this).closest('tr').find('.new-memo-sharee-input').val();
+    //console.log('nickname = '+nickname);
 
+    //만약 입력된 닉네임이 현재 공유중인 사람들 중에 있다면 얼럿을 띄우고 리턴한다.
+    let sharingGuys = $('.memo-sharer').map(function() {
+        return $(this).text();
+    }).get();
+    if (sharingGuys.includes(nickname)) {
+        Swal.fire(
+            '이런!',
+            '이미 공유된 회원에게는 메모를 다시 공유할 수 없습니다!.',
+            'error'
+        );
+        return;
+    }
+    if ($('#memo-user-nickname').val() === nickname) {
+        Swal.fire(
+            '이런!',
+            '자기 자신에게는 메모를 공유할 수 없습니다!',
+            'error'
+        );
+        return;
+    }
+
+    addMemoAccessRequest(memoNo,nickname);
+});
+//    let nickname = $(this).closest('tr').find('.new-memo-sharee-input').val();
+//new-memo-sharee-input
+$(document).on('change', '.new-memo-sharee-input', function() {
+    //지금 입력된 값
+    let nickname = $(this).val();
+    let checkSpace = $(this).closest('tr').find('.nickname-valid-check');
+    nicknameCheckRequest(nickname,checkSpace);
+});
