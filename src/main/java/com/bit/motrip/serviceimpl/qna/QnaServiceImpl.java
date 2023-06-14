07ea5ed54bfa7012350da11e59d7e70e72d7cf85
kimerum333 <kimerum333@gmail.com>
2023-06-14 17:09:23 +0900

    alex6/Update 23/06/14 17:09 질의응답답변알람
    어드민에게 질의응답이 있을시 답변이감
    
    (QnaMapper.xml, QnaServiceImpl)

--- src/main/java/com/bit/motrip/serviceimpl/qna/QnaServiceImpl.java
+++ src/main/java/com/bit/motrip/serviceimpl/qna/QnaServiceImpl.java
@@ -73,12 +73,12 @@ public class QnaServiceImpl implements QnaService {
     public void addQna(Qna qna) throws Exception {
 
         qnaDao.addQna(qna);
-
+/*
         System.out.println("질의응답의 내용을 확인합니다.");
         System.out.println("qnaNo : " + qna.getQnaNo());
         System.out.println("qnaTitle : " + qna.getQnaTitle());
         System.out.println("qnaAuthor : " + qna.getQnaAuthor());
-        System.out.println("qnaCategory : " + qna.getQnaCategory());
+        System.out.println("qnaCategory : " + qna.getQnaCategory());*/
 
         String realCategory = "";
         if(qna.getQnaCategory() == 0){
@@ -100,8 +100,8 @@ public class QnaServiceImpl implements QnaService {
         //어드민 리스트를 가져온다(미구현)
         User receiver = userService.getUserById("admin");
         User sender = userService.getUserById(qna.getQnaAuthor());
-        String alarmTitle = sender.getNickname() + "님의"+realCategory+" :"+qna.getQnaTitle();
-        String alarmContents = sender.getUserName() + "님이"+realCategory+","+qna.getQnaTitle()+"을 등록하셨습니다. 지금 확인해주세요";
+        String alarmTitle = sender.getNickname() + "님의 "+realCategory+" :"+qna.getQnaTitle();
+        String alarmContents = sender.getUserName() + "님이 "+realCategory+","+qna.getQnaTitle()+" 을 등록하셨습니다. 지금 확인해주세요";
         String alarmNaviUrl = "/qna/getQna?qnaNo="+qna.getQnaNo();
 
         alarmService.addNavigateAlarm(sender,receiver, alarmTitle, alarmContents, alarmNaviUrl);
@@ -112,6 +112,42 @@ public class QnaServiceImpl implements QnaService {
     public void addQnaAnswer(Qna qna) throws Exception {
 
         qnaDao.addQnaAnswer(qna);
+
+        Qna gettingQna = qnaDao.getQna(qna.getQnaNo());
+        //내용 확인
+/*        System.out.println("질의응답의 내용을 확인합니다.");
+        System.out.println("qnaNo : " + gettingQna.getQnaNo());
+        System.out.println("qnaTitle : " + gettingQna.getQnaTitle());
+        System.out.println("qnaAuthor : " + gettingQna.getQnaAuthor());
+        System.out.println("qnaCategory : " + gettingQna.getQnaCategory());*/
+
+        String realCategory = "";
+        if(gettingQna.getQnaCategory() == 0){
+            realCategory = "계정문의";
+        } else if (gettingQna.getQnaCategory() == 1) {
+            realCategory = "기타문의";
+        } else if (gettingQna.getQnaCategory() == 2) {
+            realCategory = "여행플랜문의";
+        } else if (gettingQna.getQnaCategory() == 3) {
+            realCategory = "채팅문의";
+        } else if (gettingQna.getQnaCategory() == 4) {
+            realCategory = "메모문의";
+        }else {
+            realCategory = "후기문의";
+        }
+
+
+
+
+        int qnaNo = gettingQna.getQnaNo();
+        User receiver = userService.getUserById(gettingQna.getQnaAuthor());
+        User fakeSender = new User();
+        fakeSender.setUserId("admin");
+
+        String alarmTitle = "문의하신 내용 "+gettingQna.getQnaTitle()+" 에 답변이 있습니다.";
+        String alarmContents = receiver.getNickname()+" 님, "+realCategory+" 사항, "+gettingQna.getQnaTitle()+" 에 답변이 등록되었습니다. 지금 확인해주세요";
+        String alarmNaviUrl = "/qna/getQna?qnaNo="+qna.getQnaNo();
+        alarmService.addNavigateAlarm(fakeSender,receiver, alarmTitle, alarmContents, alarmNaviUrl);
     }
 
     //질의응답 응답 등록 서비스
