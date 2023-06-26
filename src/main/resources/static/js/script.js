719ba6892c6141756ea76f54463e422f231c4beb
sean <tmdgus717@naver.com>
2023-06-26 20:49:06 +0900

    sean/main.js 23/06/26  지도완 화상채팅만 합시다.
    멀티 화상채팅 구현해야합니다.

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -34,7 +34,7 @@ socket.on('user-disconnected', (userId) => {
 
 myPeer.on('open', (id) => {
     console.log("My peer Id is : "+ id);
-    // socket.emit('join-room', ROOM_ID, id);
+    socket.emit('join-room', ROOM_ID, id);
 });
 
 
